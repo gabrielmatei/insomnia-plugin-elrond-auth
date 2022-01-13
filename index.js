5b9d67f5d953695c8fd1a8ac8af7dd4c1eb23a84
Gabriel Matei <gabrielvmatei@gmail.com>
2022-01-13 21:54:09 +0200

    batch id template tag

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const setAuthorizationHeader = require('./src/setAuthorizationHeader');
 const setSignature = require('./src/setSignature');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports.requestHooks = [
   setAuthorizationHeader
@@ -7,4 +8,15 @@ module.exports.requestHooks = [
 
 module.exports.responseHooks = [
   setSignature
+];
+
+module.exports.templateTags = [
+  {
+    name: 'batchId',
+    displayName: 'Generate Batch ID',
+    description: 'Generate Batch ID',
+    async run() {
+      return uuidv4();
+    }
+  }
 ];
