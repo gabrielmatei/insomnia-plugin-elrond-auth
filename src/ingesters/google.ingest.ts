2423fc4b8c1fe61cb4c6f6d73f40826c080c2262
Gabriel Matei <gabrielvmatei@gmail.com>
2022-05-19 11:48:49 +0300

    dummy google ingest

--- src/ingesters/google.ingest.ts
+++ src/ingesters/google.ingest.ts
@@ -1,7 +1,7 @@
-import moment from "moment";
-import { google, webmasters_v3 } from "googleapis";
+// import moment from "moment";
+// import { google, webmasters_v3 } from "googleapis";
 import { Ingest } from "src/crons/data-ingester/entities/ingest.interface";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotImplementedException } from "@nestjs/common";
 import { GoogleEntity } from "src/common/timescale/entities/google.entity";
 import { IngestRecords } from "src/crons/data-ingester/entities/ingest.records";
 
@@ -10,73 +10,75 @@ export class GoogleIngest implements Ingest {
   public readonly name = GoogleIngest.name;
   public readonly entityTarget = GoogleEntity;
 
-  private readonly webmasters: webmasters_v3.Webmasters = google.webmasters('v3');
+  // private readonly webmasters: webmasters_v3.Webmasters = google.webmasters('v3');
 
-  constructor() {
-    const auth = new google.auth.GoogleAuth({
-      keyFile: 'config/googleAuthConfig.json',
-      scopes: [
-        'https://www.googleapis.com/auth/webmasters',
-        'https://www.googleapis.com/auth/webmasters.readonly',
-      ],
-    });
-    google.options({ auth });
-  }
+  // constructor() {
+  //   const auth = new google.auth.GoogleAuth({
+  //     keyFile: 'config/googleAuthConfig.json',
+  //     scopes: [
+  //       'https://www.googleapis.com/auth/webmasters',
+  //       'https://www.googleapis.com/auth/webmasters.readonly',
+  //     ],
+  //   });
+  //   google.options({ auth });
+  // }
 
+  // eslint-disable-next-line require-await
   public async fetch(): Promise<IngestRecords[]> {
-    const timestamp = moment.utc().toDate();
+    throw new NotImplementedException();
+    // const timestamp = moment.utc().toDate();
 
-    const startDate = moment.utc().startOf('day').subtract(3, 'days').format('YYYY-MM-DD');
-    const endDate = moment.utc().startOf('day').subtract(2, 'days').format('YYYY-MM-DD');
-    const highlightedWords = ['elrond', 'egld', 'egold'];
+    // const startDate = moment.utc().startOf('day').subtract(3, 'days').format('YYYY-MM-DD');
+    // const endDate = moment.utc().startOf('day').subtract(2, 'days').format('YYYY-MM-DD');
+    // const highlightedWords = ['elrond', 'egld', 'egold'];
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const queryData: any = {};
+    // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // const queryData: any = {};
 
-    const { data } = await this.webmasters.searchanalytics.query({
-      siteUrl: 'https://elrond.com',
-      requestBody: {
-        startDate,
-        endDate,
-      },
-    });
+    // const { data } = await this.webmasters.searchanalytics.query({
+    //   siteUrl: 'https://elrond.com',
+    //   requestBody: {
+    //     startDate,
+    //     endDate,
+    //   },
+    // });
 
-    if (data.rows) {
-      const [searchData] = data.rows;
-      queryData.total = searchData;
-    }
+    // if (data.rows) {
+    //   const [searchData] = data.rows;
+    //   queryData.total = searchData;
+    // }
 
-    await Promise.all(
-      highlightedWords.map(async (word) => {
-        const { data } = await this.webmasters.searchanalytics.query({
-          siteUrl: 'https://elrond.com',
-          requestBody: {
-            startDate,
-            endDate,
-            dimensionFilterGroups: [
-              {
-                filters: [
-                  {
-                    dimension: 'query',
-                    operator: 'contains',
-                    expression: word,
-                  },
-                ],
-              },
-            ],
-          },
-        });
+    // await Promise.all(
+    //   highlightedWords.map(async (word) => {
+    //     const { data } = await this.webmasters.searchanalytics.query({
+    //       siteUrl: 'https://elrond.com',
+    //       requestBody: {
+    //         startDate,
+    //         endDate,
+    //         dimensionFilterGroups: [
+    //           {
+    //             filters: [
+    //               {
+    //                 dimension: 'query',
+    //                 operator: 'contains',
+    //                 expression: word,
+    //               },
+    //             ],
+    //           },
+    //         ],
+    //       },
+    //     });
 
-        if (data.rows) {
-          const [searchData] = data.rows;
-          queryData[word] = searchData;
-        }
-      })
-    );
+    //     if (data.rows) {
+    //       const [searchData] = data.rows;
+    //       queryData[word] = searchData;
+    //     }
+    //   })
+    // );
 
-    return [{
-      entity: GoogleEntity,
-      records: GoogleEntity.fromObject(timestamp, queryData),
-    }];
+    // return [{
+    //   entity: GoogleEntity,
+    //   records: GoogleEntity.fromObject(timestamp, queryData),
+    // }];
   }
 }
